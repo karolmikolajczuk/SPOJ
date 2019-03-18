b6244b79104f8353d138c2085f22abf0e0e5dff8
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-18 23:31:00 +0100

    Add Factorial task from SPOJ.

---
+++ FCTRL_3/FCTRL_3/FCTRL_3/Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FCTRL_3
+{
+    static public class Factorial
+    {
+        static public void firstTwoNumbers(UInt64 number)
+        {
+            if (number<0)
+            {
+                Console.WriteLine("Liczba mniejsza od 0");
+                return;
+            }
+
+            UInt64 factorial = 1;
+
+            switch(number)
+            {
+                case 0:
+                case 1:
+                    factorial = 1;
+                    break;
+                default:
+                    for (UInt64 i = 1; i <= number; i++) 
+                    {
+                        factorial *= i;
+                    }
+                    break;
+            }
+            Console.WriteLine("{0} {1}", factorial%100/10, factorial%10);
+        }
+    }
+}
---
+++ FCTRL_3/FCTRL_3/FCTRL_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FCTRL_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            UInt16 tests = Convert.ToUInt16(Console.ReadLine());
+            for (int i = 0; i < tests; i++)
+            {
+                Factorial.firstTwoNumbers(Convert.ToUInt64(Console.ReadLine()));
+            }
+        }
+    }
+}
