9cce9b8b5a0e0d3739395365bbfb1654fb936a37
karolmikolajczuk <eryk.mikolajczuk@gmail.com>
2019-03-18 22:54:27 +0100

    Add Prime Project from SPOJ

---
+++ PRIME/PRIME/PRIME/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRIME
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
